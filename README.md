b1c17dbdbd561feaeb39add5cd0d5ae1baa460c2
Development <>
2020-06-09 10:06:13 -0500

    update Readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # logic_Test
+
+### Descripción
+
+La prueba lógica se encuentra desarrollada en swift para un proyecto de linea de comandos mac los pasos a seguir para ejecutar el proyecto son: 
+
+* Descargar los archivos
+* Abrir el archivo "logicTest.xcodeproj", este abrirá el xcode
+* Asegurarse de que se encuentra seleccionado el target "logicTest"
+* Click en el ícono Play
+
+Posteriormente a la ejecución, ***en la consola se solicitarán la entradas, es necesario seguir las instrucciones que solicita para obtener los resultados***.
+
+_Para la resolución de este problema se tomó el enfoque de detectar el patrón de los casos posibles de las figuras que podrian formarse con la matríz tal cómo se muestra en la siguiente imagen_
+
+![alt text](https://github.com/andrescs7/logic_Test/blob/master/Captura%20de%20Pantalla%202020-06-09%20a%20la(s)%2010.01.47.png)
--- logicTest/main.swift
+++ logicTest/main.swift
@@ -9,62 +9,57 @@ import Foundation
 
 print("¿Cúantos casos se considerarán? (escriba el número y pulse enter)")
 var casos = Int(readLine() ?? "") ?? 0
-if(casos < 1 || casos > 5000)
-{
-    print("El número de casos permitidos se encuentra en un rango entre 1 y 5000")
-}
-else
-{
-    let startOn = 0
-    let endCases = casos-1
-    for i in startOn...endCases{
-        let numCase = i + 1
-        print("Caso \(numCase) - Indique el valor de N (pulse enter después de escribir)")
-        let rows = Int(readLine() ?? "") ?? 0
-        print("Caso \(numCase) - Indique el valor de M (pulse enter después de escribir)")
-        let columns = Int(readLine() ?? "") ?? 0
 
-        if (rows == columns && rows > 1) {
-            if rows % 2 == 0
-            {
-                print("L")
-                
-            }
-            else
-            {
-                print("R")
-            }
+let startOn = 0
+let endCases = casos-1
+for i in startOn...endCases{
+    let numCase = i + 1
+    print("Caso \(numCase) - Indique el valor de N (pulse enter después de escribir)")
+    let rows = Int(readLine() ?? "") ?? 0
+    print("Caso \(numCase) - Indique el valor de M (pulse enter después de escribir)")
+    let columns = Int(readLine() ?? "") ?? 0
+    
+    if (rows == columns && rows > 1) {
+        if rows % 2 == 0
+        {
+            print("L")
+            
         }
-        else if (rows > columns && columns > 1) {
-            if columns % 2 == 0
-            {
-                print("U")
-            }
-            else
-            {
-                print("D")
-            }
-        }
-        else if (columns > rows) {
-            if rows % 2 == 0
-            {
-                print("L")
-            }
-            else
-            {
-                print("R")
-            }
-        }
-        else if (columns == 1) {
-            if rows == 1
-            {
-                print("R")
-            }
-            else
-            {
-                print("D")
-            }
+        else
+        {
+            print("R")
         }
     }
+    else if (rows > columns && columns > 1) {
+        if columns % 2 == 0
+        {
+            print("U")
+        }
+        else
+        {
+            print("D")
+        }
+    }
+    else if (columns > rows) {
+        if rows % 2 == 0
+        {
+            print("L")
+        }
+        else
+        {
+            print("R")
+        }
+    }
+    else if (columns == 1) {
+        if rows == 1
+        {
+            print("R")
+        }
+        else
+        {
+            print("D")
+        }
+    }
+    
 }
 
